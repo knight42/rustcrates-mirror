a76d7f37569f38541f0c055ae0b01794ee216a09
Knight <Knight42@mail.ustc.edu.cn>
2016-06-16 15:11:37 +0800

    guarded by cluster module

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ require('babel-core/register')({
 require('babel-polyfill');
 const app = require('./app.js');
 const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log(`listening on ${port}`)
-})
+const cluster = require('cluster')
+const CPUs = require('os').cpus().length
+
+if (cluster.isMaster) {
+  for (var i = CPUs; i > 0; i--) {
+    cluster.fork()
+  }
+  cluster.on('exit', (worker, node, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log(`node: ${node}`);
+    console.log(`signal: ${signal}`);
+    cluster.fork()
+  })
+} else {
+  app.listen(port, () => {
+    console.log(`listening on ${port}`)
+  })
+}
